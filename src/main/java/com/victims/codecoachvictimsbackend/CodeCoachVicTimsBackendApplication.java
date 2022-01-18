a03820445ae93506ad7bc02179c454705ace89aa
tmassart873 <tim.massart@hotmail.com>
2022-01-18 11:05:40 +0100

    Initial commit

---
+++ src/main/java/com/victims/codecoachvictimsbackend/CodeCoachVicTimsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.victims.codecoachvictimsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCoachVicTimsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeCoachVicTimsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victims/codecoachvictimsbackend/CodeCoachVicTimsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victims.codecoachvictimsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCoachVicTimsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
