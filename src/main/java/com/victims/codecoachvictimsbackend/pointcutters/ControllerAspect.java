c800f5f48a67e38e2055026c0289c97276047c9e
Maxim <maximverbeeck@hotmail.com>
2022-01-19 09:38:40 +0100

    build(startup logging) : controlleraspect created

---
+++ src/main/java/com/victims/codecoachvictimsbackend/pointcutters/ControllerAspect.java
@@ -0,0 +1,31 @@
+package com.victims.codecoachvictimsbackend.pointcutters;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ControllerAspect {
+    private Logger logger = LoggerFactory.getLogger(ControllerAspect.class);
+
+    @Pointcut("execution(* com.victims.codecoachvictimsbackend.*.api.*.*(..))")
+    public void allControllers() {}
+
+    @Before("allControllers()")
+    public void logInfoBeforeAllControllerMethods(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().toShortString();
+        logger.info("Calling method: " + method);
+    }
+
+    @After("allControllers()")
+    public void logInfoAfterAllControllerMethods(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().toShortString();
+        logger.info("Finished method: " + method);
+    }
+}
--- src/main/java/com/victims/codecoachvictimsbackend/user/api/UserController.java
+++ src/main/java/com/victims/codecoachvictimsbackend/user/api/UserController.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping(path="users")
 public class UserController {
 
-    @GetMapping(path = "helloworld", produces = "text/html")
+    @GetMapping(path = "helloworld", produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public String helloWorld(){
         return "Hello Tim =)";
