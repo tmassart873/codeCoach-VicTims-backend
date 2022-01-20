4be207a862779b5ca458e11a860bea588a68ec75
Ben Mees <benmees@outlook.com>
2022-01-20 11:21:21 +0100

    BUILD(story2) added Entity not found exception

--- src/main/java/com/victims/codecoachvictimsbackend/user/api/ControllerExceptionalHandler.java
+++ src/main/java/com/victims/codecoachvictimsbackend/user/api/ControllerExceptionalHandler.java
@@ -1,16 +1,16 @@
 package com.victims.codecoachvictimsbackend.user.api;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @ControllerAdvice
 public class ControllerExceptionalHandler {
-    @ExceptionHandler(NullPointerException.class)
+    @ExceptionHandler({NullPointerException.class, EntityNotFoundException.class})
     protected void setNullPointerException(NullPointerException nullPointerException, HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.BAD_REQUEST.value(), nullPointerException.getMessage());
     }
