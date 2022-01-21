475a030a2c0f03a944946569983bfdf50cf01582
Maxim <maximverbeeck@hotmail.com>
2022-01-21 09:18:16 +0100

    build(finished cleaning domain User & DB): removed password column + script to remove password

--- src/main/java/com/victims/codecoachvictimsbackend/user/api/UserController.java
+++ src/main/java/com/victims/codecoachvictimsbackend/user/api/UserController.java
@@ -10,6 +10,7 @@ import com.victims.codecoachvictimsbackend.user.service.UserService;
 import org.keycloak.admin.client.Keycloak;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
