ec0ad0da31b756657cd9e9748bab1855e5a7a184
tmassart873 <tim.massart@hotmail.com>
2022-01-24 10:33:29 +0100

    test(story5): started unit tests

---
+++ src/test/java/com/victims/codecoachvictimsbackend/user/domain/CoachInformationUnitTest.java
@@ -0,0 +1,9 @@
+package com.victims.codecoachvictimsbackend.user.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class CoachInformationUnitTest {
+    @Test
+    void givenACoachee_WhenBecomingACoach_ThenTheUserRoleHasChangedToCoach() {
+    }
+}
