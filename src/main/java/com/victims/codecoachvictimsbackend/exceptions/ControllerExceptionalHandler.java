da055e1d19352f5b24bfebf27f5d1ecdc8d209d8
Dries Verreydt <johndoe@example.com>
2022-01-24 13:16:07 +0100

    fix(Story 1): ControllerExceptionalHandler fix to use responseentity

--- src/main/java/com/victims/codecoachvictimsbackend/exceptions/ControllerExceptionalHandler.java
+++ src/main/java/com/victims/codecoachvictimsbackend/exceptions/ControllerExceptionalHandler.java
@@ -1,6 +1,7 @@
 package com.victims.codecoachvictimsbackend.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,8 +11,12 @@ import java.io.IOException;
 
 @ControllerAdvice
 public class ControllerExceptionalHandler {
-    @ExceptionHandler({NullPointerException.class, EntityNotFoundException.class, UserNotFoundException.class})
+    /*@ExceptionHandler({NullPointerException.class, EntityNotFoundException.class, UserNotFoundException.class})
     protected void setNullPointerException(NullPointerException nullPointerException, HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.BAD_REQUEST.value(), nullPointerException.getMessage());
+    }*/
+    @ExceptionHandler({NullPointerException.class, EntityNotFoundException.class, UserInformationException.class, UserNotFoundException.class})
+    protected ResponseEntity<String> handleBadRequestRE(Exception exception) {
+        return new ResponseEntity<>("{\"message\":\""+exception.getMessage()+"\"}",null,HttpStatus.BAD_REQUEST.value());
     }
 }
