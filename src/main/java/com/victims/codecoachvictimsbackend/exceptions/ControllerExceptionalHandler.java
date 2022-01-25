3b5e734760aeba01e08b0e87f503604124d039a5
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-01-25 16:14:09 +0100

    BUILD(request_session) added SessionNotFoundException

--- src/main/java/com/victims/codecoachvictimsbackend/exceptions/ControllerExceptionalHandler.java
+++ src/main/java/com/victims/codecoachvictimsbackend/exceptions/ControllerExceptionalHandler.java
@@ -15,7 +15,13 @@ public class ControllerExceptionalHandler {
     protected void setNullPointerException(NullPointerException nullPointerException, HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.BAD_REQUEST.value(), nullPointerException.getMessage());
     }*/
-    @ExceptionHandler({NullPointerException.class, EntityNotFoundException.class, UserInformationException.class, UserNotFoundException.class})
+    @ExceptionHandler({
+            NullPointerException.class,
+            EntityNotFoundException.class,
+            UserInformationException.class,
+            UserNotFoundException.class,
+            SessionNotFoundException.class
+    })
     protected ResponseEntity<String> handleBadRequestRE(Exception exception) {
         return new ResponseEntity<>("{\"message\":\""+exception.getMessage()+"\"}",null,HttpStatus.BAD_REQUEST.value());
     }
