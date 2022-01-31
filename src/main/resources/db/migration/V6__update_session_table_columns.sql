8e08473a013c9d777080a502a14c69c37832b861
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-01-31 15:19:00 +0100

    alter(flyway) v6 fk_coach_id_session renamed

--- src/main/resources/db/migration/V6__update_session_table_columns.sql
+++ src/main/resources/db/migration/V6__update_session_table_columns.sql
@@ -2,4 +2,4 @@ alter table session
 rename column fk_coachee_id to coachee_id;
 
 alter table session
-add constraint fk_coach_id foreign key (coach_id) REFERENCES app_user (id);
+add constraint fk_coach_id_session foreign key (coach_id) REFERENCES app_user (id);
